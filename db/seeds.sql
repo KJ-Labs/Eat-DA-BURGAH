18a0e808ea201199fbf9ed3cc9640a13a8a47dde
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-09-17 19:58:32 -0700

    Updated files so they work with a burger framework

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Pork Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name) VALUES ('Tofu Burger');
