54aeb8fe613fcedb65d26cc66083c09e2e7c8021
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-09-19 15:29:13 -0700

    Actual Heroku update

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 connection = mysql.createConnection({
-  host: 'sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'np4f50cx1ubm9gnb',
-  password: 'wsfnkawwmslnr5wj',
-  database: 'ccngwdg2l1ut7p3t'
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'burger_db'
 });
 }; 
 
